60bfa3c1591c47d902a36fd3b76763a6a07c0e7a
Polina Krusteva <polina.krusteva.95@gmail.com>
2015-05-08 14:14:36 +0300

    Fixing cinema map

--- class_Cinema.py
+++ class_Cinema.py
@@ -6,11 +6,11 @@ class Cinema:
         self.seats_availability = {}
         # self.col = 0
         # self.row = 0
-        for i in range(0, 10):
-            for j in range(0, 10):
+        for i in range(1, 11):
+            for j in range(2, 21):
                 if self.seats[i][j] == "X":
                     self.seats_availability[(i, j)] = 1
-                else:
+                elif self.seats[i][j] == ".":
                     self.seats_availability[(i, j)] = 0
 
     def print_cinema(self):
@@ -19,22 +19,31 @@ class Cinema:
             print(ss)
 
     def is_available(self, row, col):
-        if self.seats_availability[(row, col)] == 1:
+        if self.seats_availability[(row, col*2)] == 1:
             return False
         return True
 
     def choose_seat(self, row, col):
         if row > 10 or col > 10:
             return False
-        self.seats_availability[(row, col)] = 1
-        self.seats[row][col] = "X"
+        self.seats_availability[(row, col*2)] = 1
+        self.seats[row][col*2] = "X"
         return True
+
+    def count_available_seats(self):
+        count = 0
+        for row in range(1, 11):
+            for col in range(2, 21):
+                if self.seats[row][col] == ".":
+                    count += 1
+        return count
 
 mycinema = Cinema("cinema_map.txt")
 print(mycinema.seats_availability)
 mycinema.print_cinema()
 print(mycinema.is_available(2, 3))
-print(mycinema.is_available(3, 7))
+print(mycinema.is_available(4, 8))
 print(mycinema.choose_seat(1, 3))
+print(mycinema.count_available_seats())
 print("\n")
 mycinema.print_cinema()
